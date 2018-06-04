8c354f170630f2d43c1a80ea2906c04c14e814cf
kyzhou2 <kyzhou2@illinois.edu>
2018-06-04 11:33:52 -0500

    Add files via upload

---
+++ rotator.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using VRTK;
+
+
+public class rotator : MonoBehaviour {
+
+	[SerializeField] private VRTK_ControllerEvents vrtkLeft, vrtkRight;
+	[SerializeField] private float spinSpeed = 0f;
+	[SerializeField] private Transform t;
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+
+	void Update(){
+		if(vrtkRight.gripPressed) t.Rotate (new Vector3 (0f, spinSpeed * Time.deltaTime, 0f));
+		else if(vrtkLeft.gripPressed) t.Rotate (new Vector3 (0f, -1.0f * spinSpeed * Time.deltaTime, 0f));
+
+
+	}
+
+}
---
+++ toggle.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class toggle : MonoBehaviour {
+
+	public void tog(){
+		gameObject.SetActive (!gameObject.activeSelf);
+	}
+}
